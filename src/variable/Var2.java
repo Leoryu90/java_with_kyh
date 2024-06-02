43f7d21106a067f7220b972b4df92191335149b3
Leoryu90 <thetein@gmail.com>
2024-06-02 17:48:23 +0900

    :tada: 20240602 Java-1

---
+++ src/variable/Var2.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var2 {
+
+    public static void main(String[] args) {
+        int a = 10;  //변수 선수, 변수 초기화
+        a = 20; //10 -> 20으로 변환
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+
+    }
+}
